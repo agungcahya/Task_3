b343df132c658fb966a6586c874ab9de2a6be76e
agungcahya <agungcahyailhami@students.telkomuniversity.ac.id>
2015-09-21 23:05:02 +0700

    update
    update

--- LinkedList/list.h
+++ LinkedList/list.h
@@ -1,6 +1,7 @@
 #ifndef LIST_H_INCLUDED
 #define LIST_H_INCLUDED
 
-
+void insert_after(List *L, element *p, element *prec);
+void delete_after(List *L, element *p, element *prec);
 
 #endif // LIST_H_INCLUDED
